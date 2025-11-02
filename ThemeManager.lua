98d7ab00788957594aad60db639d8b51de92ad23
fddafa <94169848+MCHUB-Trash@users.noreply.github.com>
2025-11-02 14:53:55 +0700

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -14,7 +14,7 @@ local ThemeManager = {} do
 		['BBot'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Halloween'] = { 9, httpService:JSONDecode('{"FontColor":"ff8800","MainColor":"0d0d0d","AccentColor":"ff6600","BackgroundColor":"000000","OutlineColor":"331a00"}') },
-		
+		['None'] = {9, nil}
 	}
 
 	function ThemeManager:ApplyTheme(theme)
